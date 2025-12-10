62ace717c72ed8801a093a5ae8b3592032a8db17
blitzboah <blitzboah@users.noreply.github.com>
2025-12-10 20:59:18 +0530

    e

--- src/math/movement.zig
+++ src/math/movement.zig
@@ -1,7 +1,10 @@
 const rl = @import("../raylib.zig");
+
+const speed = 2.0;
+
 pub fn update(playerPosition: *rl.c.Vector2) void {
-    if (rl.c.IsKeyDown(rl.c.KEY_W)) playerPosition.y -= 1.0;
-    if (rl.c.IsKeyDown(rl.c.KEY_A)) playerPosition.x -= 1.0;
-    if (rl.c.IsKeyDown(rl.c.KEY_S)) playerPosition.y += 1.0;
-    if (rl.c.IsKeyDown(rl.c.KEY_D)) playerPosition.x += 1.0;
+    if (rl.c.IsKeyDown(rl.c.KEY_W)) playerPosition.y -= 1.0 * speed;
+    if (rl.c.IsKeyDown(rl.c.KEY_A)) playerPosition.x -= 1.0 * speed;
+    if (rl.c.IsKeyDown(rl.c.KEY_S)) playerPosition.y += 1.0 * speed;
+    if (rl.c.IsKeyDown(rl.c.KEY_D)) playerPosition.x += 1.0 * speed;
 }
