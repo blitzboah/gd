e9f1904ed554b99cd84246555a9e68e4e7c328ea
blitzboah <blitzboah@users.noreply.github.com>
2025-12-10 21:49:50 +0530

    waowwwwwwwwwwwwwwwwww 3ddddd

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 const rl = @import("raylib.zig");
 const movement = @import("math/movement.zig");
+const vector3 = rl.c.Vector3;
 
 pub fn main() void {
     const screenWidth = 1280;
@@ -8,25 +9,37 @@ pub fn main() void {
 
     rl.c.InitWindow(screenWidth, screenHeight, "window");
     rl.c.SetTargetFPS(60);
-
     defer rl.c.CloseWindow();
 
-    var ballPosition = rl.c.Vector2{ .x = screenWidth / 2, .y = screenHeight / 2 };
+    var camera: rl.c.Camera3D = undefined;
+    camera.position = (vector3){ .x = 0, .y = 10, .z = 10 };
+    camera.target = (vector3){ .x = 0, .y = 0, .z = 0 };
+    camera.up = (vector3){ .x = 0, .y = 1, .z = 0 };
+    camera.fovy = 50;
+    camera.projection = rl.c.CAMERA_PERSPECTIVE;
+    rl.c.DisableCursor();
+
+    const boxPosition = vector3{ .x = 0, .y = 0, .z = 0 };
 
     //const fixed_dt = 1.0 / 60.0;
 
     while (!rl.c.WindowShouldClose()) {
-
-        //update
-        movement.update(&ballPosition);
+        //movement.update(&boxPosition);
+        rl.c.UpdateCamera(&camera, rl.c.CAMERA_FREE);
 
         rl.c.BeginDrawing();
         defer rl.c.EndDrawing();
 
         rl.c.ClearBackground(rl.c.BLACK);
 
-        rl.c.DrawText("e", 20, 20, 20, rl.c.WHITE);
+        rl.c.BeginMode3D(camera);
+        defer rl.c.EndMode3D();
 
-        rl.c.DrawCircleV(ballPosition, 20, rl.c.BLUE);
+        rl.c.DrawCube(boxPosition, 2, 2, 2, rl.c.BLUE);
+        rl.c.DrawCubeWires(boxPosition, 2, 2, 2, rl.c.WHITE);
+
+        rl.c.DrawGrid(10, 1);
+
+        rl.c.DrawText("e", 20, 20, 20, rl.c.WHITE);
     }
 }
--- src/math/movement.zig
+++ src/math/movement.zig
@@ -1,10 +1,10 @@
 const rl = @import("../raylib.zig");
 
-const speed = 2.0;
+const speed = 3.0;
 
-pub fn update(playerPosition: *rl.c.Vector2) void {
-    if (rl.c.IsKeyDown(rl.c.KEY_W)) playerPosition.y -= 1.0 * speed;
-    if (rl.c.IsKeyDown(rl.c.KEY_A)) playerPosition.x -= 1.0 * speed;
-    if (rl.c.IsKeyDown(rl.c.KEY_S)) playerPosition.y += 1.0 * speed;
-    if (rl.c.IsKeyDown(rl.c.KEY_D)) playerPosition.x += 1.0 * speed;
+pub fn update(playerPosition: *rl.c.Vector3) void {
+    if (rl.c.IsKeyDown(rl.c.KEY_W)) playerPosition.z -= 1.0 * speed * rl.c.GetFrameTime();
+    if (rl.c.IsKeyDown(rl.c.KEY_A)) playerPosition.x -= 1.0 * speed * rl.c.GetFrameTime();
+    if (rl.c.IsKeyDown(rl.c.KEY_S)) playerPosition.z += 1.0 * speed * rl.c.GetFrameTime();
+    if (rl.c.IsKeyDown(rl.c.KEY_D)) playerPosition.x += 1.0 * speed * rl.c.GetFrameTime();
 }
